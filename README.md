d5b70a4d668650f111d2312266f831e55b602124
unknown <724804113@qq.com>
2023-11-08 00:06:45 +0800

    update.

---
+++ README.md
@@ -0,0 +1,20 @@
+MangaDex Plugin  
+============  
+  
+A Rulia plugin for reading [MangaDex](https://mangadex.org).  
+  
+Description  
+----------  
+  
+* It works.  
+* You need to customize the Http header in the plugin configuration, with the key being `User-Agent` and the value being `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0`.
+* There are a lot of bugs, I'll fix them when I have time.
+* There are also many shortcomings; I'll make changes when I have time.
+
+Login  
+----  
+  
+You can log in in two ways (it works without logging in):  
+  
+* Manually add a header named `Cookie` in the Rulia plugin settings and fill in the Cookie.  
+* Use the web popup to log in, fill in `https://mangadex.org` as the URL, and log in successfully.
---
+++ index.js
@@ -0,0 +1,199 @@
+async function getMangaListByLatest(page, pageSize) {
+	const comicsIdUrl = 'https://api.mangadex.org/chapter';
+	const url = 'https://api.mangadex.org/manga';
+	try {
+		const comicsIdRawResponse = await window.Rulia.httpRequest({
+			url: comicsIdUrl,
+			method: 'GET',
+			payload: 'limit=' + pageSize +
+				'&offset=' + ((page - 1) * pageSize) +
+				'&includes[]=user&includes[]=scanlation_group&includes[]=manga&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&order[readableAt]=desc'
+		});
+		const comicsIdResponse = JSON.parse(comicsIdRawResponse);
+		var comicsIdList = [];
+		for (var x of comicsIdResponse.data) {
+			comicsIdList.push(x.relationships[1].id);
+		}
+		var payload = '';
+
+		for (var i = 0; i < comicsIdList.length; i++) {
+			payload += 'ids[]=' + comicsIdList[i];
+			if (i < (comicsIdList.length - 1)) {
+				payload += '&';
+			} else {
+				payload += '&includes[]=cover_art';
+			}
+		}
+		const rawResponse = await window.Rulia.httpRequest({
+			url: url,
+			method: 'GET',
+			payload: payload
+		});
+		const response = JSON.parse(rawResponse);
+		var result = {
+			list: []
+		}
+		for (manga of response.data) {
+			var comicTitle = '';
+			for (var comic in manga.attributes.title) {
+				comicTitle = manga.attributes.title[comic];
+				break;
+			}
+			const mangaDetailString = JSON.stringify(manga.relationships);
+			const regex = /"fileName":\s*"(.*?)"/;
+			const match = mangaDetailString.match(regex);
+			let fileName = match[1];
+			var comic = {
+				title: comicTitle,
+				url: 'https://www.mangadex.org/title/' + manga.id,
+				coverUrl: 'https://mangadex.org/covers/' + manga.id + '/' + fileName + '.256.jpg'
+			}
+			result.list.push(comic)
+		}
+		window.Rulia.endWithResult(result);
+	} catch (error) {
+		window.Rulia.endWithException(error.message);
+	}
+}
+
+async function getMangaListBySearching(page, pageSize, keyword) {
+	const comicsIdUrl = 'https://api.mangadex.org/manga';
+	const url = 'https://api.mangadex.org/manga';
+	try {
+		const comicsIdRawResponse = await window.Rulia.httpRequest({
+			url: comicsIdUrl,
+			method: 'GET',
+			payload: 'limit=' + pageSize + '&offset=' + ((page - 1) * pageSize) +
+				'&includes[]=cover_art&includes[]=artist&includes[]=author&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&title=' +
+				keyword + '&includedTagsMode=AND&excludedTagsMode=OR'
+		});
+		const comicsIdResponse = JSON.parse(comicsIdRawResponse);
+		var comicsIdList = [];
+		for (var x of comicsIdResponse.data) {
+			comicsIdList.push(x.id);
+		}
+		var payload = '';
+
+		for (var i = 0; i < comicsIdList.length; i++) {
+			payload += 'ids[]=' + comicsIdList[i];
+			if (i < (comicsIdList.length - 1)) {
+				payload += '&';
+			} else {
+				payload += '&includes[]=cover_art';
+			}
+		}
+		const rawResponse = await window.Rulia.httpRequest({
+			url: url,
+			method: 'GET',
+			payload: payload
+		});
+		const response = JSON.parse(rawResponse);
+		var result = {
+			list: []
+		}
+		for (manga of response.data) {
+			var comicTitle = '';
+			for (var comic in manga.attributes.title) {
+				comicTitle = manga.attributes.title[comic];
+				break;
+			}
+			const mangaDetailString = JSON.stringify(manga.relationships);
+			const regex = /"fileName":\s*"(.*?)"/;
+			const match = mangaDetailString.match(regex);
+			let fileName = match[1];
+			var comic = {
+				title: comicTitle,
+				url: 'https://www.mangadex.org/title/' + manga.id,
+				coverUrl: 'https://mangadex.org/covers/' + manga.id + '/' + fileName + '.256.jpg'
+			}
+			result.list.push(comic)
+		}
+		window.Rulia.endWithResult(result);
+	} catch (error) {
+		window.Rulia.endWithException(error.message);
+	}
+}
+
+async function getMangaList(page, pageSize, keyword) {
+	if (keyword) {
+		return await getMangaListBySearching(page, pageSize, keyword);
+	} else {
+		return await getMangaListByLatest(page, pageSize);
+	}
+}
+
+async function getMangaData(dataPageUrl) {
+	const seasonIdMatchExp = /\/([0-9a-fA-F-]+)(\/|$|\?)/;
+	const seasonIdMatch = dataPageUrl.match(seasonIdMatchExp);
+	const detailUrl = 'https://api.mangadex.org/manga/' + seasonIdMatch[1];
+	const chapterListUrl = 'https://api.mangadex.org/manga/' + seasonIdMatch[1] + '/feed';
+	try {
+		const detailRawResponse = await window.Rulia.httpRequest({
+			url: detailUrl,
+			method: 'GET',
+			payload: 'includes[]=artist&includes[]=author&includes[]=cover_art'
+		})
+		const detailResponse = JSON.parse(detailRawResponse);
+		let comicTitle = '';
+		let comicDescription = '';
+		for (var comic in detailResponse.data.attributes.title) {
+			comicTitle = detailResponse.data.attributes.title[comic];
+			break;
+		}
+		for (var comic in detailResponse.data.attributes.description) {
+			comicDescription = detailResponse.data.attributes.description[comic];
+			break;
+		}
+		const mangaDetailString = JSON.stringify(detailResponse.data.relationships);
+		const regex = /"fileName":\s*"(.*?)"/;
+		const match = mangaDetailString.match(regex);
+		let comicCover = 'https://mangadex.org/covers/' + seasonIdMatch[1] + '/' + match[1] + '.256.jpg';
+		var result = {
+			title: comicTitle,
+			description: comicDescription,
+			coverUrl: comicCover,
+			chapterList: []
+		}
+		const chapterListRawResponse = await window.Rulia.httpRequest({
+			url: chapterListUrl,
+			method: 'GET',
+			payload: 'limit=500&includes[]=scanlation_group&includes[]=user&order[volume]=desc&order[chapter]=desc&offset=0&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&contentRating[]=pornographic'
+		});
+		const chapterListResponse = JSON.parse(chapterListRawResponse);
+		for (var manga of chapterListResponse.data) {
+			var comic = {
+				title: manga.attributes.translatedLanguage + '.' + manga.attributes.chapter,
+				url: 'https://mangadex.org/chapter/' + manga.id
+			}
+			result.chapterList.push(comic);
+		}
+		window.Rulia.endWithResult(result);
+	} catch (error) {
+		window.Rulia.endWithException(error.message);
+	}
+}
+
+async function getChapterImageList(chapterUrl) {
+	const episodeIdMatchExp = /\/chapter\/([0-9a-fA-F-]+)(\/|$|\?)/;
+	const episodeIdMatch = chapterUrl.match(episodeIdMatchExp);
+	const url = 'https://api.mangadex.org/at-home/server/' + episodeIdMatch[1];
+	const rawResponse = await window.Rulia.httpRequest({
+		url: url,
+		method: 'GET',
+		payload: 'forcePort443=false'
+	});
+	const response = JSON.parse(rawResponse);
+	var result = [];
+
+	for (manga of response.chapter.data) {
+		result.push({
+			url: 'https://uploads.mangadex.org/data/' + response.chapter.hash + '/' + manga,
+			width: 1,
+			height: 1
+		});
+	}
+	window.Rulia.endWithResult(result);
+}
+async function getImageUrl(path) {
+	window.Rulia.endWithResult(path);
+}
